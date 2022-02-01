896619254ef7d7168fbacfde8ecf63a7142fae65
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2022-02-01 10:32:30 +0100

    Dit hoort bij opdracht: F1.7.01.O1 - Een gevulde repository + F1.7.01.O2 - Een lichtschakelaar: tkinter

--- lightswitch.py
+++ lightswitch.py
@@ -1,3 +1,5 @@
+from distutils import command
+from pydoc import text
 import tkinter as tk
 window = tk.Tk()
 
@@ -5,9 +7,24 @@ button = tk.Button(text='...', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
+lightstatus = "off"
+window.config(bg="black")
+button.config(text="Switch light on")
 
-
-
+def showMsg():
+    global lightstatus
+    if lightstatus == "off":
+        button.config(text="Switch light " + lightstatus)
+        lightstatus = "on"
+        window.config(bg="yellow")
+    elif lightstatus == "on":
+        button.config(text="Switch light " + lightstatus)
+        lightstatus = "off"
+        window.config(bg="black")
+    print("Light is", lightstatus)
+        
+button.config(command=showMsg)
+    
 # schijf hier tussen je code
 
 window.mainloop()
